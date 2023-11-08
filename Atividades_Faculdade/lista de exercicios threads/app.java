4aef46d436a2a054017d6c3cd866f477ce499531
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2023-11-08 01:11:11 -0300

    Atividade de SO quesitos 1 a 3 da lista de exercicio 5

---
+++ Atividades_Faculdade/lista de exercicios threads/app.java
@@ -0,0 +1,6 @@
+public class app {
+    public static void main(String[] args) {
+        Minhathread thread_1 = new Minhathread("catapimbas");
+        Minhathreadsemsleep thread_2 = new Minhathreadsemsleep("Trembolona");
+    }
+}
---
+++ Atividades_Faculdade/lista de exercicios threads/app2.java
@@ -0,0 +1,9 @@
+public class app2 {
+    public static void main(String[] args) {
+        Minhathreadsomadora thread_3 = new Minhathreadsomadora(1);
+        Minhathreadsomadora thread_4 = new Minhathreadsomadora(3);
+        Minhathreadsomadora thread_5 = new Minhathreadsomadora(5);
+        Minhathreadsomadora thread_6 = new Minhathreadsomadora(7);
+        Minhathreadsomadora thread_7 = new Minhathreadsomadora(9);
+    }
+}
